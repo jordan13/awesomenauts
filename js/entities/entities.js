22dfcdfa462b1024ae0d60fd72875c40310bbd13
Jordan Abujaber <jajaber13@gmail.com>
2015-01-30 08:55:05 -0800

    Actual code for video 16, last push was video 15

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -91,13 +91,18 @@
  	    		var ydif = this.pos.y - response.b.pos.y;
  	    		var xdif = this.pos.x - response.b.pos.x;
 
- 	    		if(xdif>-35 && this.facing=== 'right' && (xdif<0)){
+ 	    		if(ydif<-40 && xdif< 70 && xdif>-35){
+ 	    			this.body.falling = false;
+ 	    			this.body.vel.y = -1;
+ 	    	     }
+ 	    		else if(xdif>-35 && this.facing=== 'right' && (xdif<0) && ydif>-50){
  	    			this.body.vel.x = 0;
  	    			this.pos.x = this.pos.x -1;
- 	    		}else if (xdif<70 && this.facing==='left' && xdif>0) {
+ 	    		}else if(xdif<70 && this.facing==='left' && xdif>0) {
  	    			this.body.vel.x = 0;
  	    			this.pos.x = this.pos.x +1;
- 	    		}
+
+ 	    	   }
  	    	}
  	    }
    });
