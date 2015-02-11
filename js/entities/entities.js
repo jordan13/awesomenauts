1b59c4620ca99d4684b9be738b9a8105b8dddf25
Jordan Abujaber <jajaber13@gmail.com>
2015-02-11 09:06:27 -0800

    video 21

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -142,7 +142,7 @@ game.PlayerBaseEntity = me.Entity.extend ({
     this.renderable.addAnimation("idle", [0]);
     this.renderable.addAnimation("broken", [1]);
     this.renderable.setCurrentAnimation("idle");
-    this.type = "PlayerBaseEntity";
+    this.type = "PlayerBase";
 
 
 },
@@ -157,6 +157,10 @@ game.PlayerBaseEntity = me.Entity.extend ({
   	this._super(me.Entity, "update", [delta]);
   	return true;
 
+  },
+
+  loseHealth: function(damage){
+    this.health = this.health - damage;
   },
 
   onCollision: function(){
@@ -227,7 +231,10 @@ game.EnemyCreep = me.Entity.extend({
 	  }]);
 		this.health = 10;
 		this.alwaysUpdate = true;
-
+    this.now = new Date().getTime(); 
+    this.attacking = false;
+    this.lastAttacking = new Date().getTime();
+    this.lastHit = new Date().getTime();
 		this.body.setVelocity(3, 20);
 
 		this.type = "EnemyCreep";
@@ -237,18 +244,37 @@ game.EnemyCreep = me.Entity.extend({
 	},
 
 	update: function(delta){
+    this.now = new Date().getTime();
     this.body.vel.x -= this.body.accel.x * me.timer.tick;
 
+    me.collision.check(this, true, this.collideHandler.bind(this), true);
 
 
     this.body.update(delta);
  
     this._super(me.Entity, "update", [delta]);
     
-
-
     return true;
-	}
+	},
+
+   loseHealth: function(damage){
+    this.health = this.health - damage;
+   },
+
+  collideHandler: function(response) {
+    if(response.b.type === 'PlayerBase') {
+      this.attacking=true;
+      this.lastAttacking=this.now;
+      this.body.vel.x = 0;
+      this.pos.x = this.pos.x + 1;
+        if((this.now-this.lastHit >= 1000)){
+          this.lastHit = this.now;
+          response.b.loseHealth(1);
+        }
+
+    }
+  }
+
 });
 
 game.GameManager = Object.extend ({
