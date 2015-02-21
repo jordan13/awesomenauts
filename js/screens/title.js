83296b000407f0bd481d7ec8752a249bc710b39d
Jordan Abujaber <jajaber13@gmail.com>
2015-02-20 18:25:02 -0800

    video 26 commented

--- js/screens/title.js
+++ js/screens/title.js
@@ -14,3 +14,6 @@ game.TitleScreen = me.ScreenObject.extend({
 		; // TODO
 	}
 });
+// titlejs is where we will have all the code for the start screen
+//we want our screen to start at the top left corner
+//the -10 sets the screen way in the back
