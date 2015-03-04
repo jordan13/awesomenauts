b46d039204b6cbdff67fd8e656d14f79bf8e6610
Jordan Abujaber <jajaber13@gmail.com>
2015-03-03 18:00:46 -0800

    Video 37 commented

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -69,7 +69,7 @@ game.GameTimerManager = Object.extend ({
 
           }
 
-          console.log(gmae.data.exp);
+          console.log(game.data.exp);
          
           return true;
 
@@ -86,3 +86,16 @@ game.GameTimerManager = Object.extend ({
 // we have refactored the game manager as much as possible
 
 // There are some other things that we can also refactor
+
+// this.gameOver is a quick fix for all the madness that is going on 
+
+//this.game over is the global variable we are using to call the game over screen
+
+//We only get into each of those if statements if game over is false
+
+
+// if it is true we can not add any more points
+
+
+// we need to apply this.gameOver to something that will call the game over clean
+
