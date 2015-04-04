1a5c05b106a4004dc2d42068560a426231a399e7
Jordan Abujaber <jajaber13@gmail.com>
2015-04-04 12:23:35 -0700

    Video 58

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -9,10 +9,12 @@
         update: function () {
           if(game.data.win === true && !this.gameover){
             this.gameOver(true);
+            alert("YOU WIN!!!");
           }else if(game.data.win === false && !this.gameover) {
            this.gameOver(false);
+           alert("HAHA YOU LOSE!!!");
           }
-           
+
           return true;
 
       },
@@ -25,9 +27,32 @@
         }
          this.gameover = true;
          me.save.exp = game.data.exp;
-      }
 
+      $.ajax({
+        type: "POST", 
+        url: "php/controller/save-user.php",
+        data: {
+          exp: game.data.exp,
+          exp1: game.data.exp1,
+          exp2: game.data.exp2,
+          exp3: game.data.exp3,
+          exp4: game.data.exp4,
+        },
 
+        dataType: "text"
+      })
+
+      .success(function(response){
+        if(response==="true"){
+          me.state.change(me.state.MENU);
+        }else{
+          alert(response);
+        }
+      })
+        .fail(function(response){
+          alert("Fail");
+      });
+  }
     });
 
 
