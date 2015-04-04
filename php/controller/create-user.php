c0cd004c953ba619e9cf1b9d1f4506ef8891bea5
Jordan Abujaber <jajaber13@gmail.com>
2015-04-04 09:16:27 -0700

    Video 56 commented

--- php/controller/create-user.php
+++ php/controller/create-user.php
@@ -28,9 +28,24 @@
   }else {
   	echo"<p>" . $_SESSION["connection"]->error . "</p>";
   }
-    // we are telling the crypt function to use the password and salt together
-    // we are telling it to use huge random numbers to create unique id's
-    // uniqid creates unique ids for us and makes it random 
-    // the minimum we should use is 5000 rounds
-    // we are going to split this up because php reads the $'s as variables'
+// we are telling the crypt function to use the password and salt together
+
+// we are telling it to use huge random numbers to create unique id's
+
+// uniqid creates unique ids for us and makes it random 
+
+// the minimum we should use is 5000 rounds
+
+// we are going to split this up because php reads the $'s as variables'
    
+// We are using ajax to call our files on the fly
+
+// ajax is a part of jquery
+
+// we need regular jquery and jquery ui
+
+// we bind the mainmenu in indexphp so when we click it, it starts a function
+
+// ajax is a way for us to update our database
+
+// we set it so that if you register a new user you can go straight to playing
