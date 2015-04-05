5ac515272fada7c5a2a1b0293993399feea38933
Jordan Abujaber <jajaber13@gmail.com>
2015-04-04 18:02:37 -0700

    Vide i6

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,4 +1,17 @@
 game.MiniMap = me.Entity.extend({
+	init: function(x, y, settings){
+		this._super(me.Entity, "init", [x, y,{
+			image: "minimap",
+			width: 1053, 
+			height: 98,
+			spritewidth: "1053",
+			spriteheight: "98",
+			getShape: function(){
+				return (new me.Rect(0, 0, 1053, 98)).toPolygon();
+			}
+		}]);
 
+		this.floating = true;
+	}
 });
 
