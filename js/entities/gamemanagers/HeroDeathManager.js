31355852c2253eedbdb37e307adf586fa24fc2d5
Jordan Abujaber <jajaber13@gmail.com>
2015-04-04 19:36:41 -0700

    Video i8 DONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- js/entities/gamemanagers/HeroDeathManager.js
+++ js/entities/gamemanagers/HeroDeathManager.js
@@ -9,6 +9,7 @@
   update: function(){
     if(game.data.player.dead) {
        me.game.world.removeChild(game.data.player);
+       me.game.world.removeChild(game.data.miniPlayer);
        me.state.current().resetPlayer(10, 0);
 
       }
